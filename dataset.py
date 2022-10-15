45c284a60b3e2d827ee7e1d896ba9f94bdd6ad87
HuangBugWei <b06501042@g.ntu.edu.tw>
2022-10-15 14:35:30 +0800

    update dataset.py

--- dataset.py
+++ dataset.py
@@ -11,15 +11,13 @@ class SeqClsDataset(Dataset):
         data: List[Dict],
         vocab: Vocab,
         label_mapping: Dict[str, int],
-        max_len: int,
-        train: bool
+        max_len: int
     ):
         self.data = data
         self.vocab = vocab
         self.label_mapping = label_mapping
         self._idx2label = {idx: intent for intent, idx in self.label_mapping.items()}
         self.max_len = max_len
-        self.train = train
 
     def __len__(self) -> int:
         return len(self.data)
@@ -27,23 +25,31 @@ class SeqClsDataset(Dataset):
     def __getitem__(self, index) -> Dict:
         instance = self.data[index]
         # instance = {'text': ['switch the language setting over to german', 'how would i go about setting up a direct deposit'], 'intent': ['change_language', 'direct_deposit'], 'id': ['train-345', 'train-1907']}
-        if self.train:
-            return instance['text'], self.collate_fn(instance)
-        else:
-            return instance['text'], instance['id']
+        # if self.train:
+        #     return instance['text'], self.collate_fn(instance)
+        # else:
+        #     return instance['text'], instance['id']
+
+        return instance
     @property
     def num_classes(self) -> int:
         return len(self.label_mapping)
 
     def collate_fn(self, samples: List[Dict]) -> Dict:
         # TODO: implement collate_fn
+        # samples: [{'text': 'make a reminder to schedule a tire check', 'intent': 'schedule_maintenance', 'id': 'train-7692'}, {'text': 'can you please talk faster', 'intent': 'change_speed', 'id': 'train-831'}]
         
-        # x = self.vocab.encode_batch([samples["text"].split()])
-        # print(x)
+        x = []
+        y = []
+        z = []
+        for sample in samples:
+            x.append(sample['text'])
+            y.append(self.label2idx(sample['intent']))
+            z.append(sample['id'])
         
-        y = self.label2idx(samples['intent'])
+        x = self.vocab.encode_batch([sentence.split() for sentence in x])
         
-        return y
+        return x, y, z
 
     def label2idx(self, label: str):
         return self.label_mapping[label]
